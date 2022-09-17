b5bf8d2a30777e2594914b1297554af32a4a75e8
JapxD <garciajapxd@gmail.com>
2022-09-17 23:37:16 +0800

    QR-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QrComponent } from './qr/qr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QrComponent
   ],
   imports: [
     BrowserModule
